c986c333d36472e9ebbc82600efc4f2a027c880a
deaddrunkspirit <dedrsp@gmail.com>
2025-05-12 00:27:36 +0200

    Fix Vite config and index.html for correct JS MIME type

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,9 @@ export default defineConfig({
   server: {
     port: 3000,
     host: true,
+    headers: {
+      'Content-Type': 'application/javascript',
+    },
   },
   build: {
     target: 'esnext',
